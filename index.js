77e024b06a8b7c5707f2734745e20e419f76457e
Zaheer khan <newzk16117541@gmail.com>
2023-03-30 23:24:38 +0530

    Adding Parcel code, Function component, JSX

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // <h1>Hello World, from React
 const header = React.createElement('h1', {}, 'Hello World, from React');
 
@@ -10,6 +12,7 @@ const heading1 = React.createElement(
   'h1',
   {
     id: 'heading1',
+    key: 1,
   },
   'Heading 1'
 );
@@ -18,6 +21,7 @@ const heading2 = React.createElement(
   'h2',
   {
     id: 'heading2',
+    key: 2,
   },
   'Heading 2'
 );
@@ -26,13 +30,62 @@ const container = React.createElement(
   'div',
   {
     id: 'container',
+    key: 3,
   },
   [heading1, heading2]
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(header);
-root.render(container);
+//root.render(header);
+// root.render(container);
 
-//Bundlers : Vite, webpack, parcel
+//Bundlers : Vite, webpack, parcel✅✅
+/**
+ * Hot module replacement (HRM)
+ * File Watcher algorithm
+ * Bundling
+ * minify
+ * Clearing our code
+ * dev and production build
+ * super fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible for all and older versions of browsers
+ * Created a server.
+ */
+
+// JSX : It use babel to convert JSX code into JavaScript
+const heading = (
+  <h1 id="header" className="header" key="1">
+    Hello world!
+  </h1>
+);
+
+// Function component
+
+const HeadComponents = () => {
+  return (
+    <>
+      {heading}
+      <h3>from the javascript element but inside the function component</h3>
+    </>
+  );
+};
+
+const heading3 = (
+  <React.Fragment>
+    <h1 id="header" className="header" key="1">
+      Inside React Component
+    </h1>
+    {/* To write js inside jsx use -> {} */}
+    {1 + 2 + ' Adding two number inside jsx code'}
+    {/* Calling Js function normally inside the JSX */}
+    {HeadComponents()}
+    {/* Calling Js function in JSX in form of JSX */}
+    <HeadComponents />
+  </React.Fragment>
+);
+
+root.render(heading3);
